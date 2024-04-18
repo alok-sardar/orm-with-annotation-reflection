3ff765e96c92b6de548607193f632c35150a3451
alok-sardar <sardaralok15@gmail.com>
2024-04-18 12:03:45 +0530

    finished constructing the sql query.

--- com/orm/app/implementation/HibernateOrm.java
+++ com/orm/app/implementation/HibernateOrm.java
@@ -10,6 +10,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class HibernateOrm<T> {
     Connection connection;
@@ -22,32 +25,24 @@ public class HibernateOrm<T> {
 
     public void write(T t) throws IllegalAccessException {
         Class<?> aClass = t.getClass();
-        String tableName=aClass.isAnnotationPresent(Table.class)?aClass.getAnnotation(Table.class).name():"";
-        StringBuilder query=new StringBuilder().append("insert into ").append(tableName);
-        StringBuilder columnNames= new StringBuilder().append("(");
-        StringBuilder values= new StringBuilder().append("(");
+        String tableName=aClass.isAnnotationPresent(Table.class)?
+                        !aClass.getAnnotation(Table.class).name().isEmpty()?aClass.getAnnotation(Table.class).name(): aClass.getSimpleName()
+                                                : aClass.getSimpleName();
+        StringBuilder query=new StringBuilder().append("insert into ").append(tableName).append("(");
         List<Field> declaredFields = Arrays.stream(aClass.getDeclaredFields()).toList();
+        StringJoiner columnJoiner=new StringJoiner(",");
+        String primaryKey;
         for (Field declaredField : declaredFields) {
-            String primaryKey="";
-            String columnName="";
+            String columnName;
             if(declaredField.isAnnotationPresent(PrimaryKey.class)){
                 primaryKey = declaredField.getAnnotation(PrimaryKey.class).name();
-                columnNames.append(primaryKey).append(",");
-                values.append(declaredField.get(t)).append(",");
             }else if(declaredField.isAnnotationPresent(Column.class)){
                 columnName=declaredField.getAnnotation(Column.class).name();
-                if(declaredFields.indexOf(declaredField)==declaredFields.size()-1){
-                    columnNames.append(columnName).append(")");
-                    values.append(declaredField.get(t)).append(")");
-                }else{
-                    columnNames.append(columnName).append(",");
-                    values.append(declaredField.get(t)).append(",");
+                columnJoiner.add(columnName);
                 }
             }
-
-        }
-        query.append(columnNames).append(" ").append(values);
-        System.out.println();
-        System.out.println(query);
+        int length = columnJoiner.toString().split(",").length;
+        String qMarks = IntStream.range(0, length).mapToObj(e -> "?").collect(Collectors.joining(","));
+        query.append(columnJoiner).append(")").append("values(").append(qMarks).append(")");
     }
 }
